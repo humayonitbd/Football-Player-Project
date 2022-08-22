629ed74d791dcc0abf294e1d8b6517bfb5529b53
humayonitbd <humayonforid44@gmail.com>
2022-08-22 21:54:37 +0600

    total calculate

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-
-
+// array element ol list push korar function
 const arrayName = [];
 
 const olList = document.getElementById('olList');
@@ -13,12 +12,9 @@ function displayName(names){
             olList.appendChild(li);
 
     }
-   
-
 }
 
-
-
+//btn seclect function
 function selectBtn(xyz){
     const selectName = xyz.parentNode.parentNode.children[0].innerText;
     if(arrayName.length >= 5){
@@ -26,48 +22,42 @@ function selectBtn(xyz){
     }else{
         arrayName.push(selectName);
         xyz.classList.add('disabled');
-        
     }
     displayName(arrayName);
-    
 }
 
-
+//input field value seclect function
 function selectIdValue(id){
     const inputField = document.getElementById(id);
     const playerTotal = parseFloat(inputField.value);
-    if(isNaN(playerTotal)){
-        alert('Please type of number !!');
-        return;
-
-    }else{
-        return playerTotal;
-    }
-    
-    
+    return playerTotal;
 }
-
+//output er id seclect function
 function outputText(output){
     const playerExpress = document.getElementById(output);
     return playerExpress;
 }
 
+//total player seclect function
+function totalPlayer(){
+    const inputField = selectIdValue('inputField') * arrayName.length;
+    return inputField;
+}
+
 document.getElementById('calculate').addEventListener('click', function(){
-    const inputField = selectIdValue('inputField') * 5;
+    const inputField = totalPlayer();
     outputText('playerExpress').innerText = `Player Expenses : ${inputField}`;
-    // inputField.value = '';
-    
 })
 
-
 document.getElementById('calculateTotal').addEventListener('click', function(){
-    const inputField = selectIdValue('inputField') * 5;
+    const inputField = totalPlayer();
     const managerPrice = selectIdValue('managerPrice');
     const coachPrice = selectIdValue('coachPrice');
     const calculateTotalPrice = inputField + managerPrice + coachPrice;
     outputText('totalPriceId').innerText = `${calculateTotalPrice}`;
 
 })
+
 
 
 

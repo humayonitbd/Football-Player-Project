0805c631869b4e617616d0bacecd62b35b147ab5
humayonitbd <humayonforid44@gmail.com>
2022-08-22 13:23:52 +0600

    javascript code select btn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
-console.log('ami')
+
+
+const arrayName = [];
+
+const olList = document.getElementById('olList');
+
+function displayName(names){
+    olList.innerText = '';
+    for (let name of names) {
+        const element = name;
+        const li = document.createElement('li');
+            li.innerText = `${element}`;
+            olList.appendChild(li);
+
+    }
+   
+
+}
+
+
+
+function selectBtn(xyz){
+    console.log(selectNam1);
+    const selectName = xyz.parentNode.parentNode.children[0].innerText;
+    
+    if(arrayName.length >= 5){
+        alert('Not selected your player')
+    }else{
+        arrayName.push(selectName);
+        
+    }
+    displayName(arrayName);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+

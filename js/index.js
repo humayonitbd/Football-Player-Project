4dc7c5dacdf6443b67b6a05d12595131f1e859f8
humayonitbd <humayonforid44@gmail.com>
2022-08-22 16:47:10 +0600

    calculation finish

--- js/index.js
+++ js/index.js
@@ -20,13 +20,12 @@ function displayName(names){
 
 
 function selectBtn(xyz){
-    console.log(selectNam1);
     const selectName = xyz.parentNode.parentNode.children[0].innerText;
-    
     if(arrayName.length >= 5){
         alert('Not selected your player')
     }else{
         arrayName.push(selectName);
+        xyz.classList.add('disabled');
         
     }
     displayName(arrayName);
@@ -34,7 +33,41 @@ function selectBtn(xyz){
 }
 
 
+function selectIdValue(id){
+    const inputField = document.getElementById(id);
+    const playerTotal = parseFloat(inputField.value);
+    if(isNaN(playerTotal)){
+        alert('Please type of number !!');
+        return;
 
+    }else{
+        return playerTotal;
+    }
+    
+    
+}
+
+function outputText(output){
+    const playerExpress = document.getElementById(output);
+    return playerExpress;
+}
+
+document.getElementById('calculate').addEventListener('click', function(){
+    const inputField = selectIdValue('inputField') * 5;
+    outputText('playerExpress').innerText = `Player Expenses : ${inputField}`;
+    // inputField.value = '';
+    
+})
+
+
+document.getElementById('calculateTotal').addEventListener('click', function(){
+    const inputField = selectIdValue('inputField') * 5;
+    const managerPrice = selectIdValue('managerPrice');
+    const coachPrice = selectIdValue('coachPrice');
+    const calculateTotalPrice = inputField + managerPrice + coachPrice;
+    outputText('totalPriceId').innerText = `${calculateTotalPrice}`;
+
+})
 
 
 
